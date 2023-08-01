408e4ebecebc324e0572645eb3147e97a36baccf
Aeius <psb6604@gmail.com>
2023-08-01 11:06:08 +0900

    settings

--- publicData/src/main/java/org/publicData/service/JWTService.java
+++ publicData/src/main/java/org/publicData/service/JWTService.java
@@ -11,7 +11,6 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.TokenExpiredException;
-import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
 @Service
