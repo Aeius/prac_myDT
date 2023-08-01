17c886b1273e895c740dead4e2f7b4eb1e39de47
박진호 <jinhoo26@gmail.com>
2023-08-01 11:22:42 +0900

    sout없애기

--- single_sign_on/src/main/java/com/single_sign_on/service/SignOnService.java
+++ single_sign_on/src/main/java/com/single_sign_on/service/SignOnService.java
@@ -34,10 +34,10 @@ public class SignOnService {
 
         //3. 키를 사용해 이 Mac 객체를 초기화
         hasher.init(secretKey);
-        System.out.println("----------"+data.get("name")+data.get("num")+data.get("num2"));
-        String hashData = data.get("name")+data.get("num")+data.get("num2");
+
+        String hmacData = data.get("name")+data.get("num")+data.get("num2");
         //3. 암호화 하려는 데이터의 바이트의 배열을 처리해 MAC 조작을 종료
-        byte[] hash = hasher.doFinal(hashData.getBytes());
+        byte[] hash = hasher.doFinal(hmacData.getBytes());
         signOnRepository.save(SignOn.builder().name(data.get("name")).CI(Base64.encodeBase64String(hash)).build());
         //4. Base 64 Encode to String
         return Base64.encodeBase64String(hash);
